a84f24396628143a365191e7aab714b903e18643
Henry Kaufman <hkaufman19@gmail.com>
2017-07-10 20:47:54 -0600

    :computer: :open_hands: Super mega redux additions

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,24 @@ const { Router } = require('express');
 
 const router = Router();
 
-const exampleData = {
-  data: [
-    {
-      id: 1,
-      name: 'Tara Simmmons',
-      email: 'tara.simmmons55@example.com'
-    },
-    {
-      id: 2,
-      name: 'Willie Romero',
-      email: 'willie.romero57@example.com'
-    },
-    {
-      id: 3,
-      name: 'Holly Barnes',
-      email: 'holly.barnes11@example.com'
-    }
-  ]
-};
+const exampleData = [
+  {
+    id: 1,
+    name: 'Tara Simmmons',
+    email: 'tara.simmmons55@example.com'
+  },
+  {
+    id: 2,
+    name: 'Willie Romero',
+    email: 'willie.romero57@example.com'
+  },
+  {
+    id: 3,
+    name: 'Holly Barnes',
+    email: 'holly.barnes11@example.com'
+  }
+];
 
-router.get('/data', (req, res) => res.send(exampleData));
+router.get('/users', (req, res) => res.send(exampleData));
 
 module.exports = router;
